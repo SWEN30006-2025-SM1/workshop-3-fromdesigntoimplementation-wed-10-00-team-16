54fc2f94a05a3b4df40b526f61e86152069f1582
Qin Yu <qcchon@student.unimelb.edu.au>
2025-03-26 12:22:19 +1100

    add assignment, file and subject class

--- app/src/main/java/com/unimelb/swen30006/workshops/Assignment.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Assignment.java
@@ -1,11 +1,11 @@
 package com.unimelb.swen30006.workshops;
 
-// Git -> Commit then push !!!
-// test
-
-
 // Sample private file class to be replaced by your implementaiton
 class File {
+    private Date createdDate;
+    private String fileData;
+    private String fileName;
+
     public String fileType(){
         double num = Math.random();
         if(num<0.5){
@@ -13,5 +13,13 @@ class File {
         } else {
             return "docx";
         }
+    }
+
+    public Int fileSize(){
+        // implement
+    }
+
+    public String content(){
+        // implement
     }
 }
---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Subject.java
@@ -0,0 +1,28 @@
+package com.unimelb.swen30006.workshops;
+
+import java.util.HashMap;
+
+/**
+ * A class that represents all validation errors for a given
+ * file, storing the errors as a key value mapping
+ */
+class Subject {
+    private String name;
+    private String subjectCode;
+
+    /**
+     * creates assignment when called
+     */
+    public void createAssignment(){
+        //implement function
+    }
+
+    /**
+     * deletes passed in assignment
+     */
+    public void deleteAssignment(String name){
+        //implement function
+    }
+
+
+}
